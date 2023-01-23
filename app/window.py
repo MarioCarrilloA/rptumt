2ff04a237f5b9fbd2cf1721b453de634466f8859
Mario Carrillo <mario.alfredo.carrillo.arevalo@ibmt.fraunhofer.de>
2023-01-23 14:40:36 +0100

    Clean up
    Signed-off-by: Mario Carrillo <mario.alfredo.carrillo.arevalo@ibmt.fraunhofer.de>

--- app/window.py
+++ app/window.py
@@ -14,7 +14,6 @@ import random
 import string
 import pyqtgraph
 
-#VERSION = "Cam_display v0.10"
 
 import sys, time, threading, cv2
 try:
@@ -56,8 +55,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 Signal = QtCore.pyqtSignal
 Slot = QtCore.pyqtSlot
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
 
 SAMPLING_TIME = 1
@@ -85,97 +82,21 @@ def grab_images(cam_num, queue):
         else:
             print("Error: can't grab camera image")
             break
-    #self.log_msg(logging.INFO, "stop capturing frames")
     cap.release()
-    #self.log_msg(logging.INFO, "releasing")
-
-
-
-
-## Signals need to be contained in a QObject or subclass in order to be correctly
-## initialized.
-##
-#class Signaller(QtCore.QObject):
-#    signal = Signal(str, logging.LogRecord)
-#
-#
-## You specify the slot function to do whatever GUI updates you want. The handler
-## doesn't know or care about specific UI elements.
-##
-#class QtHandler(logging.Handler):
-#    def __init__(self, slotfunc, *args, **kwargs):
-#        super(QtHandler, self).__init__(*args, **kwargs)
-#        self.signaller = Signaller()
-#        self.signaller.signal.connect(slotfunc)
-#
-#    def emit(self, record):
-#        s = self.format(record)
-#        self.signaller.signal.emit(s, record)
-
-#
-# This example uses QThreads, which means that the threads at the Python level
-# are named something like "Dummy-1". The function below gets the Qt name of the
-# current thread.
-#
-
-
-
-#def ctname():
-#    return QtCore.QThread.currentThread().objectName()
-
-
-#
-# Used to generate random levels for logging.
-#
-LEVELS = (logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR,
-          logging.CRITICAL)
-
-
-# This example worker just outputs messages sequentially, interspersed with
-# random delays of the order of a few seconds.
-#
-#class Worker(QtCore.QObject):
-#    @Slot()
-#    def start(self):
-#        extra = {'qThreadName': ctname() }
-#        logger.debug('Started work', extra=extra)
-#        i = 1
-#        # Let the thread run until interrupted. This allows reasonably clean
-#        # thread termination.
-#        while not QtCore.QThread.currentThread().isInterruptionRequested():
-#            delay = 0.5 + random.random() * 2
-#            time.sleep(delay)
-#            level = random.choice(LEVELS)
-#            logger.log(level, 'Message after delay of %3.1f: %d', delay, i, extra=extra)
-#            i += 1
-#
 
 
 class MainWindow(QMainWindow, guiApp):
-
-    COLORS = {
-        logging.DEBUG: 'blue',
-        logging.INFO: 'black',
-        logging.WARNING: 'orange',
-        logging.ERROR: 'red',
-        logging.CRITICAL: 'purple',
-    }
-
     def __init__(self, app):
         super().__init__()
         self.setupUi(self)
 
-        ################################# SIGNALS
         f = QtGui.QFont('nosuchfont')
         f.setStyleHint(f.Monospace)
         self.console.setFont(f)
         self.console.setReadOnly(True)
-        #self.handler = h = QtHandler(self.update_status)
         # Remember to use qThreadName rather than threadName in the format string.
         fs = '%(asctime)s %(qThreadName)-5s %(levelname)-8s %(message)s'
         formatter = logging.Formatter(fs)
-        #h.setFormatter(formatter)
-        #logger.addHandler(h)
         # Set up to terminate the QThread when we exit
         app.aboutToQuit.connect(self.force_quit)
 
@@ -184,17 +105,13 @@ class MainWindow(QMainWindow, guiApp):
         layout.addWidget(self.console)
 
         # Connect the non-worker slots and signals
-        #self.liveview_button.clicked.connect(self.manual_update)
 
         # Start a new worker thread and connect the slots for the worker
         self.start_thread()
-        #self.sampling_button.clicked.connect(self.worker.start)
         self.sampling_button.clicked.connect(self.get_sample)
 
         # DISABLE BUTTON
         # Once started, the button should be disabled
-        #self.sampling_button.clicked.connect(lambda : self.sampling_button.setEnabled(False))
-
 
         # MONITORING
         self.monitor_timer = QTimer(self)
@@ -207,16 +124,12 @@ class MainWindow(QMainWindow, guiApp):
         self.msu_line = self.status_chart.plot(self.data_x, self.data_y, name="measurements",
                     pen='b', symbol='o', symbolSize=5)
 
-        #legend.addItem(self.ref_line, name=self.ref_name.opts['beads'])
         self.monitor_queue = Queue.Queue()
-
-#
         self.liveview_enabled = False
 
         # Init view
         self.show_default_view()
         self.console.log_msg(logging.INFO, "initialized system")
-
 
         # Sample items
         self.sampled_images = {}
@@ -430,12 +343,6 @@ class MainWindow(QMainWindow, guiApp):
         #self.capture_thread.join()
 
 
-#######################################################################################################
-#######################################################################################################
-#######################################################################################################
-#######################################################################################################
-
-
     def start_thread(self):
         self.worker = Worker()
         self.worker_thread = QtCore.QThread()
@@ -459,40 +366,4 @@ class MainWindow(QMainWindow, guiApp):
         # For use when the window is closed
         if self.worker_thread.isRunning():
             self.kill_thread()
-
-#    # The functions below update the UI and run in the main thread because
-#    # that's where the slots are set up
-#
-#    @Slot(str, logging.LogRecord)
-#    def update_status(self, status, record):
-#        color = self.COLORS.get(record.levelno, 'black')
-#        s = '<pre><font color="%s">%s</font></pre>' % (color, status)
-#        self.console.appendHtml(s)
-#
-#
-#    @Slot()
-#    def log_msg(self, level, msg):
-#        # This function uses the formatted message passed in, but also uses
-#        # information from the record to format the message in an appropriate
-#        # color according to its severity (level).
-#        #level = random.choice(LEVELS)
-#        extra = {'qThreadName': ctname() }
-#        logger.log(level, msg, extra=extra)
-#
-#
-#
-#
-#    @Slot()
-#    def manual_update(self):
-#        # This function uses the formatted message passed in, but also uses
-#        # information from the record to format the message in an appropriate
-#        # color according to its severity (level).
-#        level = random.choice(LEVELS)
-#        extra = {'qThreadName': ctname() }
-#        logger.log(level, 'Manually logged!', extra=extra)
-#
-#    @Slot()
-#    def clear_display(self):
-#        self.console.clear()
-#
 
