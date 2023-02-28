f91eec98fc700290aa4be1d3534a4fe52423e2aa
Mario Carrillo <mario.alfredo.carrillo.arevalo@ibmt.fraunhofer.de>
2023-02-28 15:00:47 +0100

    Add option to calibrate pH7 with buffer
    Signed-off-by: Mario Carrillo <mario.alfredo.carrillo.arevalo@ibmt.fraunhofer.de>

--- utils/pH_data/get_pH_sensor_data.py
+++ utils/pH_data/get_pH_sensor_data.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 import signal
 import sys
+import click
 
 from colorama import init as colorama_init
 from colorama import Fore
@@ -19,9 +20,12 @@ class serialpHSensor():
 
     def readline_cr(self):
         EOL = b'\r'
+        #EOL = b'3'
         line = bytearray()
         while True:
+            print("reading")
             c = self.serial_port.read(1)
+            #print(c)
             if c:
                 line += c
                 if line[-len(EOL):] == EOL:
@@ -32,28 +36,38 @@ class serialpHSensor():
 
 
     def command(self, cmd):
-        self.serial_port.write(cmd.encode("UTF-8"))
-        time.sleep(0.2)
-        data = self.readline_cr()
+        print("Executed command: " + cmd)
+        #self.serial_port.write(cmd.encode("UTF-8"))
+        self.serial_port.write(cmd.encode())
+        #self.serial_port.write(cmd)
+        #time.sleep(60)
+        #data = self.readline_cr()
+        data = self.serial_port.readline()
+        #return data.decode("UTF-8")
+        return data.decode()
+        #print(data)
+        #data = self.serial_port.readline()
+        #print(data)
+
         return data
 
     def start_calibration(self):
-        msg = self.command("CLR!\r")
+        msg = self.command('CLR!\r')
         return msg
+
+    def init_pH7_calibration(self):
+        msg = self.command('113!\r')
+        return msg
+
+    def retrive_single_pH_value(self):
+        msg = self.command('999!\r')
+        return msg
+
 
     def end_calibration(self):
-        msg = self.command("QIT!\r")
+        msg = self.command('QIT!\r')
         return msg
 
-
-
-    def start_redundant_calibration(self):
-        data = self.loop_cmd("CLR!\r")
-        if (len(data) != 0):
-            print("Measured voltage: " + data)
-        else:
-            print("Measured voltage: UNKNOWN!")
-    
 
     # Possible baudrate
     # - 9600
@@ -64,11 +78,13 @@ class serialpHSensor():
             port='/dev/ttyUSB0',
             baudrate=115200,
             parity=serial.PARITY_NONE,
-            bytesize=8,
-            stopbits=1,
-            timeout=5
+            bytesize=serial.EIGHTBITS,
+            stopbits=serial.STOPBITS_ONE,
+            timeout=10
         )
-        self.print_id()
+        print("Serial port open:" + str(self.serial_port.is_open))
+        print("\n")
+        #self.print_id()
 
 
     def loop_cmd(self, cmd):
@@ -85,40 +101,61 @@ class serialpHSensor():
 def main():
     colorama_init()
     s = serialpHSensor()
+    s.init_control()
 
     while True:
         print(f"{Fore.CYAN}1. Start Calibration{Style.RESET_ALL}!")
         print(f"{Fore.CYAN}2. End Calibration{Style.RESET_ALL}!")
         print(f"{Fore.CYAN}3. Init pH 7 calibration{Style.RESET_ALL}!")
+        print(f"{Fore.CYAN}4. Retrive single pH value{Style.RESET_ALL}!")
         print(f"{Fore.CYAN}5. Measure pH values (loop){Style.RESET_ALL}!")
-        print(f"{Fore.CYAN}5. Exit{Style.RESET_ALL}!\n")
+        print(f"{Fore.CYAN}6. Exit{Style.RESET_ALL}!\n")
         option = input("What would you like to do?: ")
+        print("Selected option:" + option)
         if option == "1":
             print(f"\n{Fore.GREEN}Starting calibration...{Style.RESET_ALL}!\n")
             msg = s.start_calibration()
             if len(msg) == 0:
-                print(f"\n{Fore.RED}error: unsuccessful init calibration!{Style.RESET_ALL}!\n")
+                print(f"\n{Fore.RED}error: no response init calibration!{Style.RESET_ALL}!\n")
             else:
-                print(msg)
-                print("done")
+                print("Response: " + str(msg))
+            pass
 
-        if option == "2":
+        elif option == "2":
             print(f"\n{Fore.GREEN}End calibration...{Style.RESET_ALL}!\n")
             msg = s.end_calibration()
             if len(msg) == 0:
-                print(f"\n{Fore.RED}error: unsuccessful end calibration!{Style.RESET_ALL}!\n")
+                print(f"\n{Fore.RED}error: no response for end calibration!{Style.RESET_ALL}!\n")
             else:
-                print(msg)
-                print("done")
+                print("Response: " + str(msg))
 
-
-        elif option == "2":
-            print(f"\n{Fore.GREEN}Init pH 7{Style.RESET_ALL}!\n")
+            pass
 
         elif option == "3":
-            print(f"\n{Fore.GREEN}Startibg loop to measure values{Style.RESET_ALL}!\n")
+            print(f"\n{Fore.GREEN}Init pH 7{Style.RESET_ALL}!\n")
+            msg = s.init_pH7_calibration()
+            if len(msg) == 0:
+                print(f"\n{Fore.RED}error: no response from init ph7 calibration!{Style.RESET_ALL}!\n")
+            else:
+                print("Response: " + str(msg))
+
 
         elif option == "4":
+            print(f"\n{Fore.GREEN}Retrive pH value{Style.RESET_ALL}!\n")
+            msg = s.retrive_single_pH_value()
+            if len(msg) == 0:
+                print(f"\n{Fore.RED}error: no response for retrive pH value!{Style.RESET_ALL}!\n")
+            else:
+                print("Response: " + str(msg))
+            print("Waiting for stabilization")
+            time.sleep(10)
+
+
+        elif option == "5":
+            print(f"\n{Fore.GREEN}Starting loop to measure values{Style.RESET_ALL}!\n")
+
+        elif option == "6":
+            s.serial_port.close()
             print(f"\n{Fore.GREEN}Exit!{Style.RESET_ALL}!\n")
             break
 
@@ -126,6 +163,9 @@ def main():
             print(f"\n{Fore.RED}error: unknown option!{Style.RESET_ALL}!\n")
 
 
+        print("\n")
+        x = input("press a key to continue...")
+        click.clear()
 
 if __name__ == "__main__":
     main()
