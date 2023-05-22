653a0eca77b84f549220962fa3d459baacf17eeb
Mario Carrillo <mario.alfredo.carrillo.arevalo@ibmt.fraunhofer.de>
2023-05-22 10:02:12 +0200

    Update scripts for detection
    Signed-off-by: Mario Carrillo <mario.alfredo.carrillo.arevalo@ibmt.fraunhofer.de>

--- utils/training/job.sh
+++ utils/training/job.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-#SBATCH --gres=gpu:V100:1 # select a host with a Volta GPU
-#SBATCH -t 30
 
-rhrk-singularity pytorch_22.10-py3.simg --bind $(pwd):/mnt pwd
+cd "yolov5"
+pip install -r requirements.txt
+pip install opencv-python-headless
+pip install tensorboard==2.4.1
+pip install protobuf==3.20.1
+echo "running YOLO!!!!!"
+echo "---------->"
+
+#python train.py --data 3DSCC.yaml  --img 640 --cache
+#python train.py --data 3DSCC.yaml --cfg yolov5n.yaml --img 640 --cache
+#python train.py --data 3DSCC.yaml --cfg yolov5s.yaml --img 640 --cache
+#python train.py --data 3DSCC.yaml --cfg yolov5m.yaml --img 640 --cache
+#python train.py --data 3DSCC.yaml --cfg yolov5l.yaml --img 640 --cache
+python train.py --data 3DSCC.yaml --cfg yolov5x.yaml --img 640 --cache
